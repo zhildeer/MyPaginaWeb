b8726ae1e50987d07bd5285a96eade1de313ed2c
zhildeer <ernestogatoale@gmail.com>
2016-06-24 23:54:45 -0500

    mi pagina web

---
+++ mision.php
@@ -0,0 +1,30 @@
+<html> 
+<head> 
+<title>Test</title> 
+<style type="text/css"> 
+#izquierda { 
+float: left; 
+width: 33%; 
+background-color: darkgreen; 
+} 
+
+#derecha { 
+float: right; 
+width: 33%; 
+background-color: transparent; 
+} 
+
+
+</style> 
+</head> 
+<body> 
+
+<div id="derecha"> 
+	<a class="navbar-brand" href="#" ><img src="iconos/facebook.png" width="40px" ></a>
+	<a class="navbar-brand" href="#" ><img src="iconos/pinterest.png" width="40px" ></a>
+</div> 
+
+
+
+</body> 
+</html>
